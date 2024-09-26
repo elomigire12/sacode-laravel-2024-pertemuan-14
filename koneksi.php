e3a32d86ed475e92087d962dd06f0d5c18ed3874
Elomi Gire <elomi.gire.78@gmail.com>
2024-09-26 20:03:01 +0900

    file tambah pertemuan 14 lanjutan ke 15

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "database_sekolah";
+
+$koneksi = mysqli_connect($host,$username,$password, $database);
+
+mysqli_select_db($koneksi,$database);
